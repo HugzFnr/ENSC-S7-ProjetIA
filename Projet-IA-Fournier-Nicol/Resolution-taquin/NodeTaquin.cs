8b6c6d4808c79f259e4fcf33dc26a91ffcd3a9a9
HugzFnr <hfournier001@ensc.fr>
2019-10-15 10:18:51 +0200

    préparation de la classe NodeTaquin

--- Projet-IA-Fournier-Nicol/Resolution-taquin/NodeTaquin.cs
+++ Projet-IA-Fournier-Nicol/Resolution-taquin/NodeTaquin.cs
@@ -9,11 +9,35 @@ namespace Resolution_taquin
     class NodeTaquin : GenericNode
     {
         public int tailleTaquin { get; private set; }
-        public int[][] state { get; set; }
+        public int[,] state { get; set; } //a bord state is a [,] array with -1 if the cell is empty
 
         public NodeTaquin (int taille)
         {
-            //state = new int[taille][taille];
+            state = new int[taille,taille];
+        }
+
+        public override bool IsEqual(GenericNode N2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetArcCost(GenericNode N2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool EndState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<GenericNode> GetListSucc()
+        {
+            throw new NotImplementedException();
+        }
+        public override double CalculeHCost()
+        {
+            throw new NotImplementedException();
         }
     }
 }
